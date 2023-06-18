7f7c323c7701c365dcdce2c45706565bfb7868c0
Javier Cruz <ingenierojaviercruz95@outlook.com>
2023-06-18 15:13:21 -0600

    aplicación de estilos a la pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+const SYSTEM_THEMES = {
+  'ra': 'theme-ra',
+  'valladolid': 'theme-valladolid',
+};
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'challenge';
+
+  constructor() {
+    this.changeTheme(SYSTEM_THEMES.valladolid);
+
+    // setTimeout(() => {
+    //   this.changeTheme(SYSTEM_THEMES.ra);
+    // }, 5000);
+  }
+
+  changeTheme(system: string) {
+    const themeCurrent = this.findThemeCurrent();
+    this.getElementRef().classList.remove(themeCurrent);
+    this.getElementRef().classList.add(system);
+  }
+
+  findThemeCurrent() {
+    if (this.getElementRef().classList.contains(SYSTEM_THEMES.ra)) {
+      return SYSTEM_THEMES.ra;
+    } else if (this.getElementRef().classList.contains(SYSTEM_THEMES.valladolid)) {
+      return SYSTEM_THEMES.valladolid;
+    } else {
+      return SYSTEM_THEMES.ra;
+    }
+  }
+
+  getElementRef() {
+    return document.documentElement;
+  }
 }
